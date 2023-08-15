71fd8a0f27ce69f5f91acd4845afb6273bd7802d
susan56789 <mwihaki2susan@gmail.com>
2023-08-15 05:51:59 +0300

    props destructuring

--- globalmantics/src/App.js
+++ globalmantics/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header'
 function App() {
   return (
     <div className="container">
-      <Header />
+      <Header subtitle="Providing Houses All Over The World" />
     </div>
   );
 }
--- globalmantics/src/components/Header.js
+++ globalmantics/src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './main.css'
-function Header() {
+const Header = ({ subtitle }) => {
     return (
         <header className="row">
             <div className='col-md-5'>
                 <img src='#' className="logo" alt="logo" />
             </div>
             <div className='col-md-7 mt-5 subtitle'>
-                Providing Houses All Over The World
+                {subtitle}
             </div>
         </header>
     )
