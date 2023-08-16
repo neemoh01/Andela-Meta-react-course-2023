2cd623a5c34b2d161736686a7981a116a0ae5cab
susan56789 <mwihaki2susan@gmail.com>
2023-08-16 05:04:20 +0300

    fetch house data from json file

--- globalmantics/src/App.js
+++ globalmantics/src/App.js
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
 import './style.css';
 import Header from './components/Header'
 
 function App() {
+
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(() => {
+    const FetchHouses = async () => {
+      const resp = await fetch("/house.json");
+      const houses = await resp.json();
+      setAllHouses(houses);
+    };
+    FetchHouses();
+  }
+    , [])
+
   return (
     <div className="container">
       <Header subtitle="Providing Houses All Over The World" />
