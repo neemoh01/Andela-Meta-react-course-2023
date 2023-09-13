1603c53cb62adf8246313c7609b1c63266175bf8
susan56789 <devnimoh@gmail.com>
2023-09-13 04:26:00 +0300

    week 5

---
+++ quiz-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+import Quiz from './Quiz.js'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Quiz />
+      </>
+    )
+  }
+}
+
+export default App
